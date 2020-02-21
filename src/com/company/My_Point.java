69c417e1b14c9e9a24bb503a9627045c73fc889d
glchernenko1 <glebchernenko1@gmail.com>
2020-02-21 19:46:14 +0300

    Add new class MyPoint

---
+++ src/com/company/My_Point.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class My_Point {
+    private double x, y;
+
+    public My_Point(){
+        x=0;
+        y=0;
+    }
+
+    public My_Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public My_Point(My_Point in){
+        x=in.x;
+        y=in.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void move(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double distance(My_Point in){
+        return Math.sqrt(Math.pow(x-in.x, 2)+Math.pow(y-in.y, 2));
+    }
+
+    public My_Point getLocation(){
+        return this;
+    }
+
+    public boolean equals(My_Point in){
+        return x==in.x && y==in.y;
+    }
+
+    @Override
+    public String toString() {
+        return "My_Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+
+}
